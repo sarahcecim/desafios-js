5c4328c2e8241dd97382df399f46860239e6f80f
Sarah.dev <111818953+sarahcecim@users.noreply.github.com>
2024-01-19 12:40:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+alert('Olá, seja bem vindo!');
+let dia = prompt('Qual é o dia da semana?')
+
+if (dia == 'sábado') {
+    alert('Bom fim de semana!');
+} else if (dia == 'domingo'){
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!')
+}
